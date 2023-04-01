eda6d01765ea94e55c196cf3478eb1d3442854bf
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-04-02 01:05:11 +0200

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,29 @@
 <!-- src/routes/+layout.svelte -->
 <script lang="ts">
-	import '../styles.css';
+    import "../styles.css";
+    import { invalidate } from "$app/navigation";
+    import { onMount } from "svelte";
+    import type { LayoutData } from "./$types";
+
+    export let data: LayoutData;
+
+    $: ({ supabase } = data);
+
+    onMount(() => {
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange(() => {
+            invalidate("supabase:auth");
+        });
+
+        return () => subscription.unsubscribe();
+    });
 </script>
 
 <svelte:head>
-	<title>User Management</title>
+    <title>User Management</title>
 </svelte:head>
 
 <div class="container" style="padding: 50px 0 100px 0">
-	<slot />
+    <slot />
 </div>
