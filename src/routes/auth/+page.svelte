fbdefb81d941211872867c5773083881eee995c1
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-04-02 00:48:19 +0200

    update

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,27 +1,9 @@
 <!-- src/routes/+page.svelte -->
 <script lang="ts">
-    import { browser } from "$app/environment";
-    import {
-        PUBLIC_SUPABASE_ANON_KEY,
-        PUBLIC_SUPABASE_URL,
-    } from "$env/static/public";
-    import type { Database } from "$lib/database.types";
-    import { createSupabaseLoadClient } from "@supabase/auth-helpers-sveltekit";
-    import type { SupabaseClient } from "@supabase/supabase-js";
     import type { PageData } from "./$types";
 
     export let data: PageData;
     export let email: string;
-
-    let supabase: SupabaseClient<Database> | undefined;
-    if (browser) {
-        supabase = createSupabaseLoadClient<Database>({
-            supabaseUrl: PUBLIC_SUPABASE_URL,
-            supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-            event: { fetch },
-            serverSession: data.session,
-        });
-    }
 </script>
 
 <svelte:head>
@@ -33,7 +15,7 @@
         <button
             class="btn btn-primary btn-block"
             on:click={() => {
-                supabase?.auth.signInWithOAuth({
+                data.supabase.auth.signInWithOAuth({
                     provider: "google",
                     options: {
                         redirectTo: `${data.url}/logging-in?redirect=/`,
@@ -52,7 +34,7 @@
         <button
             class="btn btn-primary btn-block"
             on:click={async () => {
-                await supabase?.auth.signInWithOtp({
+                await data.supabase.auth.signInWithOtp({
                     email,
                     options: {
                         emailRedirectTo: `${data.url}/logging-in?redirect=/`,
--- src/routes/logging-in/+page.svelte
+++ src/routes/logging-in/+page.svelte
@@ -20,8 +20,5 @@
 </script>
 
 <section>
-    "Because as we know, there are known knowns; there are things we know we
-    know. We also know there are known unknowns; that is to say we know there
-    are some things we do not know. But there are also unknown unknowns—the ones
-    we don't know we don't know" - Donald Rumsfeld
+    Loading...
 </section>
